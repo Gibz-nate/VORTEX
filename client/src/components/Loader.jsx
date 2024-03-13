004bb1dbf0b3f40a653003b7f1d794d480cfd1d2
gibz <gibsonate123@gmail.com>
2024-03-13 19:50:25 +0300

    loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,15 @@
-const Loader = () => (
-    <div className="flex justify-center items-center py-3">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700" />
+const Loader = (
+  
+) => (
+    <div className="flex flex-col justify-center items-center py-3">
+      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-cyan-700" />
+      <div className="text-white mt-3" >
+        <p>Connecting to  METAMASK.....</p>
+        <p className="text-orange-500">--Finish Setup on METAMASK Wallet--</p>
+      </div>
+
     </div>
+    
   );
   
   export default Loader;
